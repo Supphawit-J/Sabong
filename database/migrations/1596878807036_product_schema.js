071f74cff73c6bf2ab5edcf1afb115b45018a4f2
walax31 <walax31009@gmail.com>
2020-08-11 16:53:34 +0700

    edit button back

--- database/migrations/1596878807036_product_schema.js
+++ database/migrations/1596878807036_product_schema.js
@@ -7,12 +7,12 @@ class ProductSchema extends Schema {
   up () {
     this.create('products', (table) => {
       table.increments()
-      table.string('username',10).notNullable().unique()
-      table.string('type',60).notNullable()
+      table.string('username',10)
+      table.string('type',60)
       table.integer('quantity', 120).notNullable()
       table.integer('price', 10).notNullable()
-      table.string('detail',60).notNullable()
-      table.string('code',60).notNullable()
+      table.string('detail',60)
+      table.string('code',60)
       table.string('name-product',60).notNullable()
 
       table.timestamps()
